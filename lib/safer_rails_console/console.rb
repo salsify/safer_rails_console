1394a59fd64db84396c388f3c5168454d1bb36b7
Timothy Su <timothysu@users.noreply.github.com>
2017-07-03 17:42:35 -0700

    Initial implementation

--- lib/safer_rails_console/console.rb
+++ lib/safer_rails_console/console.rb
@@ -1,20 +1,12 @@
 module SaferRailsConsole
   module Console
     class << self
-      include SaferRailsConsole::Colors
-
       def initialize_sandbox
         require 'safer_rails_console/patches/sandbox'
       end
 
       def print_warning
-        puts color_text(SaferRailsConsole.config.warn_text, SaferRailsConsole.prompt_color) # rubocop:disable Rails/Output
-      end
-
-      def sandbox_prompt_user_input
-        puts "Defaulting the console into sandbox mode.\nType 'disable' to disable. Anything else will begin a sandboxed session:" # rubocop:disable Rails/Output
-        input = gets.strip
-        input != 'disable'
+        puts SaferRailsConsole::Colors.color_text(SaferRailsConsole.config.warn_text, SaferRailsConsole.prompt_color) # rubocop:disable Rails/Output
       end
     end
   end
--- spec/safer_rails_console/console_spec.rb
+++ spec/safer_rails_console/console_spec.rb
@@ -16,22 +16,4 @@ describe SaferRailsConsole::Console do
       expect { described_class.print_warning }.to output(expected_output).to_stdout
     end
   end
-
-  context ".sandbox_prompt_user_input" do
-    context "input: 'disable'" do
-      before { allow(described_class).to receive(:gets).and_return('disable') }
-
-      it "disables the sandbox" do
-        expect(described_class.sandbox_prompt_user_input).to eq(false)
-      end
-    end
-
-    context "input: 'something else'" do
-      before { allow(described_class).to receive(:gets).and_return('something else') }
-
-      it "enables the sandbox" do
-        expect(described_class.sandbox_prompt_user_input).to eq(true)
-      end
-    end
-  end
 end
