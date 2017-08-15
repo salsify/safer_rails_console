909b27f5f4cfdbf2b9911ca9dd5186a6c281483c
Timothy Su <timothysu@users.noreply.github.com>
2017-08-15 18:43:13 -0400

    Patch PostgreSQLAdapter#execute_and_clear instead of AbstractAdapter#log for auto-rollback (#15)

--- lib/safer_rails_console/patches/sandbox/auto_rollback.rb
+++ lib/safer_rails_console/patches/sandbox/auto_rollback.rb
@@ -2,15 +2,44 @@ module SaferRailsConsole
   module Patches
     module Sandbox
       module AutoRollback
+        def self.rollback_and_begin_new_transaction
+          connection = ::ActiveRecord::Base.connection
+          connection.rollback_db_transaction
+          connection.begin_db_transaction
+        end
+
         module ActiveRecord
           module ConnectionAdapters
-            module AbstractAdapter
-              def log(sql, name = 'SQL', binds = [], statement_name = nil)
-                super(sql, name, binds, statement_name) { yield }
+            module PostgreSQLAdapter41
+              def exec_no_cache(sql, name, binds)
+                super
               rescue => e
-                connection = ::ActiveRecord::Base.connection
-                connection.rollback_db_transaction
-                connection.begin_db_transaction
+                SaferRailsConsole::Patches::Sandbox::AutoRollback.rollback_and_begin_new_transaction
+                raise e
+              end
+
+              def exec_cache(sql, name, binds)
+                super
+              rescue => e
+                SaferRailsConsole::Patches::Sandbox::AutoRollback.rollback_and_begin_new_transaction
+                raise e
+              end
+            end
+
+            module PostgreSQLAdapter42
+              def execute_and_clear(sql, name, binds)
+                super
+              rescue => e
+                SaferRailsConsole::Patches::Sandbox::AutoRollback.rollback_and_begin_new_transaction
+                raise e
+              end
+            end
+
+            module PostgreSQLAdapter5
+              def execute_and_clear(sql, name, binds, prepare: false)
+                super
+              rescue => e
+                SaferRailsConsole::Patches::Sandbox::AutoRollback.rollback_and_begin_new_transaction
                 raise e
               end
             end
@@ -21,4 +50,12 @@ module SaferRailsConsole
   end
 end
 
-::ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend(SaferRailsConsole::Patches::Sandbox::AutoRollback::ActiveRecord::ConnectionAdapters::AbstractAdapter)
+if defined?(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+  if SaferRailsConsole::RailsVersion.four_one?
+    ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(SaferRailsConsole::Patches::Sandbox::AutoRollback::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter41)
+  elsif SaferRailsConsole::RailsVersion.four_two?
+    ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(SaferRailsConsole::Patches::Sandbox::AutoRollback::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter42)
+  else
+    ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(SaferRailsConsole::Patches::Sandbox::AutoRollback::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter5)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,14 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
+  rails_root = File.join(RSpec::Core::RubyProject.root, 'spec', 'internal', "rails_#{::Rails.version[0..2].tr('.', '_')}")
+
+  config.before :suite do
+    system("cd #{rails_root} && rake db:drop && rake db:setup && rake db:test:prepare")
+  end
+
   config.before :all do
-    @rails_root = File.join(RSpec::Core::RubyProject.root, 'spec', 'internal', "rails_#{::Rails.version[0..2].tr('.', '_')}")
+    @rails_root = rails_root
     @rails_cmd = File.join(@rails_root, 'bin', 'rails')
     @rails_env = { BUNDLE_GEMFILE: File.join(@rails_root, 'Gemfile') }
   end
