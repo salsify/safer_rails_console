d27a1de24e2c5e3a4d8cd44ae9f02b6179a783c5
Timothy Su <timothysu@users.noreply.github.com>
2017-08-15 15:49:52 -0400

    style

--- spec/integration/patches/sandbox_spec.rb
+++ spec/integration/patches/sandbox_spec.rb
@@ -6,17 +6,17 @@ describe "Integration: patches/sandbox" do
   end
 
   context "auto_rollback" do
-    let(:console_commands) { ['Model.first', 'Model.where(invalid: :statement)','exit'] }
+    let(:console_commands) { ['Model.first', 'Model.where(invalid: :statement)', 'exit'] }
 
     it "automatically executes rollback and begins a new transaction after executing a invalid SQL statement" do
+      expect(cmd[:stdout]).to include('ActiveRecord::StatementInvalid')
+
       # Currently, postgres is used for CI and local development is done against SQLite3
       # TODO: Use 'dotenv' to allow developers to specify a database type
       if ENV['CI']
-        expect(cmd[:stdout]).to include('ActiveRecord::StatementInvalid')
         expect(cmd[:stderr].scan('rollback transaction').count).to eq(2)
         expect(cmd[:stderr].scan('begin transaction').count).to eq(1)
       else
-        expect(cmd[:stdout]).to include('ActiveRecord::StatementInvalid')
         expect(cmd[:stderr].scan('rollback transaction').count).to eq(1)
         expect(cmd[:stderr]).not_to include('begin transaction')
       end
