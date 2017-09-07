d3e02f5bfebf5eee9eb6bf7e886667825f85079f
Timothy Su <timothysu@users.noreply.github.com>
2017-09-07 10:28:05 -0400

    Set DB transactions to read-only and provide messaging for non-read operations (#21)

--- lib/safer_rails_console/patches/sandbox/auto_rollback.rb
+++ lib/safer_rails_console/patches/sandbox/auto_rollback.rb
@@ -2,10 +2,22 @@ module SaferRailsConsole
   module Patches
     module Sandbox
       module AutoRollback
+        extend SaferRailsConsole::Colors
+
         def self.rollback_and_begin_new_transaction
           connection = ::ActiveRecord::Base.connection
           connection.rollback_db_transaction
           connection.begin_db_transaction
+        end
+
+        def self.handle_and_reraise_exception(e)
+          if e.message.include?('PG::ReadOnlySqlTransaction')
+            puts color_text('An operation could not be completed due to read-only mode.', RED) # rubocop:disable Rails/Output
+          else
+            rollback_and_begin_new_transaction
+          end
+
+          raise e
         end
 
         module ActiveRecord
@@ -14,15 +26,13 @@ module SaferRailsConsole
               def exec_no_cache(sql, name, binds)
                 super
               rescue => e
-                SaferRailsConsole::Patches::Sandbox::AutoRollback.rollback_and_begin_new_transaction
-                raise e
+                SaferRailsConsole::Patches::Sandbox::AutoRollback.handle_and_reraise_exception(e)
               end
 
               def exec_cache(sql, name, binds)
                 super
               rescue => e
-                SaferRailsConsole::Patches::Sandbox::AutoRollback.rollback_and_begin_new_transaction
-                raise e
+                SaferRailsConsole::Patches::Sandbox::AutoRollback.handle_and_reraise_exception(e)
               end
             end
 
@@ -30,8 +40,7 @@ module SaferRailsConsole
               def execute_and_clear(sql, name, binds)
                 super
               rescue => e
-                SaferRailsConsole::Patches::Sandbox::AutoRollback.rollback_and_begin_new_transaction
-                raise e
+                SaferRailsConsole::Patches::Sandbox::AutoRollback.handle_and_reraise_exception(e)
               end
             end
 
@@ -39,8 +48,7 @@ module SaferRailsConsole
               def execute_and_clear(sql, name, binds, prepare: false)
                 super
               rescue => e
-                SaferRailsConsole::Patches::Sandbox::AutoRollback.rollback_and_begin_new_transaction
-                raise e
+                SaferRailsConsole::Patches::Sandbox::AutoRollback.handle_and_reraise_exception(e)
               end
             end
           end
