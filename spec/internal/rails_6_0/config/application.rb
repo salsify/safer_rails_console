f9a6d0b40070eb42c2eaa4c25a0bb1b5022c0ae0
Joel Turkel <jturkel@salsify.com>
2019-09-13 13:51:39 -0400

    Upgrade sample Rails app

--- spec/internal/rails_6_0/config/application.rb
+++ spec/internal/rails_6_0/config/application.rb
@@ -1,17 +1,19 @@
 require_relative 'boot'
 
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-# require 'active_storage/engine'
-require 'action_controller/railtie'
-# require 'action_mailer/railtie'
-require 'action_view/railtie'
-# require 'action_cable/engine'
-# require 'sprockets/railtie'
-# require 'rails/test_unit/railtie'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -26,8 +28,5 @@ module Rails52
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
   end
 end
--- spec/internal/rails_6_0/config/environments/development.rb
+++ spec/internal/rails_6_0/config/environments/development.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -37,10 +38,10 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
