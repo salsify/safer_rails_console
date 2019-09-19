96e5f0b5161d5cc83fd18e5836d2532836c292ec
Joel Turkel <jturkel@salsify.com>
2019-09-19 09:06:28 -0400

    Rework sandbox test to be more of a blackbox

--- spec/integration/patches/sandbox_spec.rb
+++ spec/integration/patches/sandbox_spec.rb
@@ -1,43 +1,40 @@
 describe "Integration: patches/sandbox" do
-  let(:cmd) do
-    cmd = Mixlib::ShellOut.new("#{@rails_cmd} console --sandbox", env: @rails_env, input: console_commands.join("\n"))
-    cmd.run_command
-    { stdout: cmd.stdout, stderr: cmd.stderr }
-  end
-
   context "auto_rollback" do
-    let(:console_commands) { ['Model.first', 'Model.where(invalid: :statement)', 'exit'] }
-
     it "automatically executes rollback and begins a new transaction after executing a invalid SQL statement" do
-      expect(cmd[:stdout]).to include('ActiveRecord::StatementInvalid')
+      run_console('Model.create!', 'Model.where(invalid: :statement)', 'Model.create!')
 
-      # Currently, postgres is used for CI and local development is done against SQLite3
-      # TODO: Use 'dotenv' to allow developers to specify a database type
-      if ENV['CI']
-        expect(cmd[:stderr].scan('ROLLBACK').count).to eq(2)
-        expect(cmd[:stderr].scan('BEGIN').count).to eq(1)
-      else
-        expect(cmd[:stderr].scan('rollback transaction').count).to eq(1)
-        expect(cmd[:stderr]).not_to include('begin transaction')
-      end
+      # Run a new console session to ensure the database changes were not saved
+      results = run_console('puts "Model Count = #{Model.count}"')
+      expect(results[:stdout]).to include("Model Count = 0")
     end
   end
 
   context "read_only" do
-    let(:console_commands) { ['Model.create!', 'exit'] }
+    it "enforces a read_only transaction" do
+      # Run a console session that makes some database changes
+      run_console('Model.create!', 'Model.create!')
 
-    it "enforces a read_only transaction and lets the user know that an operation could not be completed" do
+      # Run a new console session to ensure the database changes were not saved
+      results = run_console('puts "Model Count = #{Model.count}"')
+      expect(results[:stdout]).to include("Model Count = 0")
+    end
+
+    it "lets the user know that an operation could not be completed" do
+      results = run_console('Model.create!')
       # Currently, postgres is used for CI and local development is done against SQLite3
-      # TODO: Use 'dotenv' to allow developers to specify a database type
+      # TODO: We should get these warnings for all types databases not just postgres
       if ENV['CI']
-        expect(cmd[:stderr]).to include('SET TRANSACTION READ ONLY')
-        expect(cmd[:stdout]).to include('ActiveRecord::StatementInvalid')
-        expect(cmd[:stdout]).to include('An operation could not be completed due to read-only mode.')
+        expect(results[:stdout]).to include('An operation could not be completed due to read-only mode.')
       else
-        expect(cmd[:stderr]).not_to include('SET TRANSACTION READ ONLY')
-        expect(cmd[:stdout]).not_to include('ActiveRecord::StatementInvalid')
-        expect(cmd[:stdout]).not_to include('An operation could not be completed due to read-only mode.')
+        expect(results[:stdout]).not_to include('An operation could not be completed due to read-only mode.')
       end
     end
+  end
+
+  def run_console(*commands)
+    commands += ['exit']
+    cmd = Mixlib::ShellOut.new("#{@rails_cmd} console --sandbox", env: @rails_env, input: commands.join("\n"))
+    cmd.run_command
+    { stdout: cmd.stdout, stderr: cmd.stderr }
   end
 end
