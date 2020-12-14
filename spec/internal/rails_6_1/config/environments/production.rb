851101520d74b6a7ffe59755e7341255d390035e
Joel Turkel <jturkel@salsify.com>
2020-12-14 13:47:45 -0500

    Lint fix

--- spec/internal/rails_6_1/config/environments/production.rb
+++ spec/internal/rails_6_1/config/environments/production.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
