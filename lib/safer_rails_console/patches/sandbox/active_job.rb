c0f9bf7358b990644759d7352fc1f46844dbb62e
Miles Zimmerman <miles@houseninja.co>
2022-08-11 15:10:39 -0700

    try without assigning test queue adapter

--- lib/safer_rails_console/patches/sandbox/active_job.rb
+++ lib/safer_rails_console/patches/sandbox/active_job.rb
@@ -7,7 +7,7 @@ module SaferRailsConsole
         # ActiveJob
         if defined?(::ActiveJob::Base)
           Rails.application.config.active_job.queue_adapter = :test
-          ActiveJob::Base.queue_adapter = :test
+          # ActiveJob::Base.queue_adapter = :test
         end
 
         # Sidekiq
